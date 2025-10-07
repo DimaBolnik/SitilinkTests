65380109523fdb58db3af24398d9a25a2b6a2c5e
Dima Bolnik <dimabolnik@mail.ru>
2025-10-07 10:36:59 +0300

    Настройка базового класса

--- src/test/java/ru/bolnik/config/TestBase.java
+++ src/test/java/ru/bolnik/config/TestBase.java
@@ -1,4 +1,22 @@
 package ru.bolnik.config;
 
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.BeforeAll;
+
 public class TestBase {
+
+    @BeforeAll
+    static void setup() {
+        Configuration.baseUrl = "https://www.citilink.ru";
+        Configuration.browser = "chrome";
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "normal";
+        Configuration.timeout = 5000;
+
+        SelenideLogger.addListener("allure", new AllureSelenide()
+                .screenshots(true)
+                .savePageSource(false));
+    }
 }
