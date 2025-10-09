017d09592f0cd7a8d67c6064b719a4e53e927c34
Dima Bolnik <dimabolnik@mail.ru>
2025-10-09 15:17:32 +0300

    добавление класса для api тестов

---
+++ src/test/java/ru/bolnik/tests/ApiTests.java
@@ -0,0 +1,34 @@
+package ru.bolnik.tests;
+
+import io.qameta.allure.Owner;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import ru.bolnik.tests.base.ApiTestBase;
+
+import static io.restassured.RestAssured.given;
+
+@Owner("Dima Bolnik")
+public class ApiTests extends ApiTestBase {
+
+    @Test
+    @DisplayName("CT-API1: Проверка, что главная страница Citilink возвращает статус 200")
+    public void checkHomePageIsAvailable() {
+        given()
+                .spec(requestSpec)
+                .when()
+                .get("/")
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    @DisplayName("CT-API2: Проверка, что несуществующая страница возвращает 404")
+    public void checkNotFoundPageReturns404() {
+        given()
+                .spec(requestSpec)
+                .when()
+                .get("/nonexistent-page-12345")
+                .then()
+                .statusCode(404);
+    }
+}
