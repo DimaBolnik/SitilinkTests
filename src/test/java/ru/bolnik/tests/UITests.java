e6538db2b45dd2e60b5216978a2d3e2c4011edd0
Dima Bolnik <dimabolnik@mail.ru>
2025-10-10 09:33:03 +0300

    добавление класса для UI тестов

--- src/test/java/ru/bolnik/tests/UITests.java
+++ src/test/java/ru/bolnik/tests/UITests.java
@@ -1,4 +1,29 @@
 package ru.bolnik.tests;
 
-public class UITests {
+import io.qameta.allure.Owner;
+import org.junit.jupiter.api.*;
+import ru.bolnik.tests.base.TestBase;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@Owner("Dima Bolnik")
+public class UITests extends TestBase {
+
+    @Test
+    @Order(1)
+    @DisplayName("CT-T13: Проверка отображения главных элементов главной страницы")
+    public void checkMainElementsOnHomePageTest() {
+        homePage.openPage();
+        homePage.checkMainElements();
+    }
+
+    @Test
+    @Order(2)
+    @DisplayName("CT-T15: Проверка отображения информации о товаре на странице продукта")
+    public void checkProductInfoOnProductPageTest() {
+        homePage.openPage();
+        homePage.searchProduct(config.searchQuery());
+        homePage.openFirstProduct();
+
+        productPage.checkProductInfo();
+    }
 }
